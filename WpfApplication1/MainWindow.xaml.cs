1c5cc04d15159a3bfaa284091187b4de4692c499
Kr1an <Kr1an@hotmail.com>
2016-11-17 09:38:18 +0300

    +animation

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public String Text { get; set; }
+		public double doubl { get; set; }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			doubl = 4.3;
+			this.DataContext = this;
+			doubl = 5.6;
+			this.Text = "asd";
+			doubl = 124.14;
+
+		}
+
+		private void TextBlock_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			doubl = 124.14;
+			InitializeComponent();
+
+		}
+
+		private void button_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			doubl = 43.26;
+			InitializeComponent();
+		}
+	}
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private MainWindow rootElement;
+		public MainWindow()
+		{
+			this.rootElement = this;
+			this.DataContext = this;
+			InitializeComponent();
+		}
+
+		private void Bordertwo_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			rootElement.Content = new Window1(rootElement).Content;
+		}
+		public MainWindow(MainWindow rootElement)
+		{
+			this.rootElement = rootElement;
+			InitializeComponent();
+			this.DataContext = this;
+
+
+		}
+	}
+}
